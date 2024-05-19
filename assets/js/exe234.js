f43512ac7fc66794a3852f1ec4d07a9ee0cd671a
Mohib-Digital <mohibullahafridi2020@gmail.com>
2024-05-19 08:01:13 -0700

    add few js code

---
+++ assets/js/exe234.js
@@ -0,0 +1,33 @@
+// alert('Mohib Ullah')
+
+// window.alert("Class: BsCs")
+
+// document.write("I am a brillient guy")
+
+// console.log("Hey Mohib Ullah Whtspp")
+
+// var myStory = "Once there was a cow"
+// console.log(myStory)
+
+// var firstName = "Mohib"
+// var lastName = "Ullah"
+
+// console.log(firstName)
+// console.log(lastName)
+
+// console.log(firstName + lastName)
+
+// var myName = "Mohib Ullah"
+// var myAge = 21
+
+// console.log(myName)
+// console.log(myAge)
+
+// var cancatenation = 2 + 4
+// console.log(cancatenation)
+
+var myCancatenation = "2" + 4
+console.log(myCancatenation)
+// Buhat maja agea yaaaaaaaaaaaaaaaaaar
+
+
