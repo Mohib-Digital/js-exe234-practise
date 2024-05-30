b13e0c6850087d48bf95d39f63a0cca0a96127cc
Mohib-Digital <mohibullahafridi2020@gmail.com>
2024-05-30 09:01:57 -0700

    add pictures and codes statement

--- assets/js/exe234.js
+++ assets/js/exe234.js
@@ -1,3 +1,13 @@
+// Chapter No: 02
+
+// var is a container for storing different values 
+
+// variable for string 
+
+// {The area between opening and closing curle brases is called a block}
+
+// different btw = , ==, !==
+
 // alert('Mohib Ullah')
 
 // window.alert("Class: BsCs")
@@ -11,31 +21,175 @@
 
 // var firstName = "Mohib"
 // var lastName = "Ullah"
-
 // console.log(firstName)
 // console.log(lastName)
 
 // console.log(firstName + lastName)
 
-// var myName = "Mohib Ullah"
-// var myAge = 21
 
+// var myName = "Mohib Ullah"
 // console.log(myName)
-// console.log(myAge)
+
+// console.log("Peshawari")
 
 // var cancatenation = 2 + 4
 // console.log(cancatenation)
 
-var myCancatenation = "2" + 4
-console.log(myCancatenation)
+// var myCancatenation = "2" + 4
+// console.log(myCancatenation)
 // Buhat maja agea yaaaaaaaaaaaaaaaaaar
 
-var csWithAlam;
-csWithAlam = "A good one";
-console.log(csWithAlam)
+// var csWithAlam;
+// csWithAlam = "A good one";
+// console.log(csWithAlam)
 
 // var jOn = "Hey jOn";
 // alert(jOn)
 // console.log("jOn")
+
+// var myName = "Mohib Ullah"
+// alert(myName)
+
+// myName = "Salarzai"
+// alert(myName)
+
+// var myName;
+// myName = "Pakistani"
+// alert(myName)
+
+// var thanx = "Thanks for your input!"
+// alert(thanx)
+
+// // =====================================================================================
+
+// Chapter No: 03
+
+// variable for number 
+// "var 1stPresident1stPresident" ===> must be wrong 
+// alert(144) "or"
+// var caseOty = 144
+// alert(caseOty)
+
+var myAge = 15;
+console.log(myAge)
+// =======================================================================================
+
+myAge = 15 + 7;
+console.log(myAge)
+//========================================================================================
+
+var myOriginalNumber = 470;
+// sorry not 470, it's 484.
+var ownNumber = myOriginalNumber + 14;
+console.log(ownNumber)
+// ========================================================================================
+
+var myfstNumber = 12;
+var myLastNumber = 8;
+var newNumber = myfstNumber + myLastNumber;
+console.log(newNumber)
+// console.log(myfstNumber + myLastNumber)
+// ========================================================================================
+
+var cardNumber = 13;
+cardNumber = cardNumber + 17;
+console.log(cardNumber)
+// ========================================================================================
+
+// cancatenation making 
+var originalNum = "23"
+var newNum = originalNum + 7
+console.log(newNum)
+// ========================================================================================
+
+// cancatenation making 
+var firstName = "Mohib Ullah"
+var secondName = "Afridi"
+var fullName = firstName + " " + secondName
+console.log(fullName)
+// ========================================================================================
+
+// Chapter No: 04 Variable Name Legal and Illegal
+
+// a variable name can't contain any space.
+// For Example: my name, full name
+// var my Name = "Mohib Ullah" // illegal
+// var myName = "Mohib Ullah" // legal
+
+// a variable can contain only letters, numbers, doller sings and underscores.
+// For Example: myName, myName1, my1Name, student10Name, my$Name, my_name, _name
+// var myName = "Mohib Ullah"
+// var myName1 = "Mohib Ullah"
+// var my1Name = "Mohib Ullah"
+// var student10Name = "Mohib Ullah"
+// var my$Name = "Mohib Ullah"
+// var my_name = "Mohib Ullah"
+// var _name = "Mohib Ullah"
+
+//Though a variable name can't be any javascript's keywords, it can contain keywords.
+// For Example myVar, userAlert are legal.
+// var var = "Mohib Ullah" // illegal
+// var myvar = "Mohib Ullah"
+// console.log(myvar)
+
+// Capital letters are fine, but be careful. Variable names are case sensitive. 
+// For Example: A "rose" is not a "Rose".
+// var rose = "Beautiful Flawer"
+// console.log(Rose)  illegal or wrong considered
+// console.log(rose) legal or correct considered
+
+// camelCase 🐪 naming
+// For Example: myFirstName, firstName, theThirdOne etc
+
+// snake_case 🐍 naming
+// For Example: first_name, my_first_name
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
